0ab43f7e8593c3e909bf8b1412f0e665827dbb7e
Remigiusz Ruszkiewicz <r.ruszkiewicz@microsoft.wsei.edu.pl>
2019-09-19 18:35:58 +0200

    Add project files.

---
+++ ArayList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SortedList collection = new SortedList();
+            collection.Add("Tytus", "Romek");
+            collection.Add("Anna", 56);
+            collection.Add("9", "x");
+
+            if (collection.ContainsKey("Tytus"))
+            {
+                Console.WriteLine("Romek");
+            }
+            //Console.WriteLine(item[]);
+            //Console.WriteLine(item[]);
+            foreach (DictionaryEntry item in collection)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+
+            Console.Read();
+            
+        }
+    }
+}
